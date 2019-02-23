0b0870dd711113910e905fe7debc8088f40d2448
Felice Forby <feliceforby@gmail.com>
2019-02-23 04:28:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,3 +31,18 @@ function getCommits(item) {
   req.open('GET', 'https;//api.github.com/repos/morinoko/' + name + '/commits');
   req.send();
 }
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+      '<li><strong>' +
+      commit.author.login +
+      '</strong> - ' +
+      commit.commit.message +
+      '</li>'
+    ).join('')}</ul>`;
+
+  document.getElementById('commits').innerHTML = commitList;
+}

de3af6ca6fbdf994bbcb8c3d1080e952bb888ca8
Felice Forby <feliceforby@gmail.com>
2019-02-23 03:52:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/morinoko/repos');
+  req.open('GET', 'https://api.github.com/users/morinoko');
   req.send();
 }

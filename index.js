6037a5b9b2d66afcfd95bf7976e1f9198a26ccc3
Felice Forby <feliceforby@gmail.com>
2019-02-23 04:38:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
-function getCommits(item) {
-  const name = item.dataset.repo;
+function getCommits(el) {
+  const name = el.dataset.repo;
   const req = new XMLHttpRequest();
 
   req.addEventListener('load', showCommits);

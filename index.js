1dc9caf5b896b18b1940e225f7fe4b416ea5c805
Felice Forby <feliceforby@gmail.com>
2019-02-23 04:43:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function getCommits(el) {
   const req = new XMLHttpRequest();
 
   req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/morinoko/' + name + '/commits');
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
   req.send();
 }
 

27f7a4b5f74ea7caefcb92f553627b4f3d90943b
Felice Forby <feliceforby@gmail.com>
2019-02-23 04:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ function showCommits() {
   const commitsList = `<ul>${commits
     .map(
       commit =>
-      '<li><strong>' +
-      commit.author.login +
-      '</strong> - ' +
-      commit.commit.message +
-      '</li>'
-    ).join('')}</ul>`;
-
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
   document.getElementById('commits').innerHTML = commitsList;
 }
